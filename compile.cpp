73536d304db8c4ea3517d3ef82ce63f55bbb1580
iannisdezwart <iannis__@hotmail.com>
2020-12-30 23:52:31 +0100

    🚀 After Transpilation, The GCC Compiler Is Executed

--- compile.cpp
+++ compile.cpp
@@ -15,11 +15,21 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	const char *input_file_name = argv[1];
-	const char *output_file_name = argv[2];
+	string input_file_name = argv[1];
+	string output_file_name = argv[2];
+	string c_file_name = output_file_name + ".c";
 
 	Parser parser(input_file_name);
-	parser.print_tag(parser.root_tag);
 
-	Transpiler transpiler(parser.root_tag, output_file_name);
+	#ifdef VERBOSE
+	printf("Parsed structure:\n");
+	parser.print_tag(parser.root_tag);
+	#endif
+
+	Transpiler transpiler(parser.root_tag, c_file_name);
+
+	printf("Sucessfully transpiled, compiling...\n");
+
+	string command = "gcc " + c_file_name + " -o " + output_file_name;
+	system(command.c_str());
 }
